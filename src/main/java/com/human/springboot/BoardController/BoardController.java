2ffc9f3dabde5e096bd852a886fb3d5c44712299
jin000000000 <wlsduddl318@gmail.com>
2023-05-24 11:49:47 +0900

    BoardController 수정본

--- src/main/java/com/human/springboot/BoardController/BoardController.java
+++ src/main/java/com/human/springboot/BoardController/BoardController.java
@@ -1,5 +1,14 @@
 package com.human.springboot.BoardController;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BoardController {
+
+	@GetMapping("/board")
+    public String showList() {
+        return "board";
+    }
 
 }
